100fb1f86d618a2f9e4cb9aba57e164626b3e7d1
mauricev <maurice@fluxsoft.com>
2026-05-13 13:05:07 -0400

    minor login fix

--- start/login.php
+++ start/login.php
@@ -1,11 +1,9 @@
-
-<!DOCTYPE html>";
 <?php
 	//include_once('../classes/session.php');
 	$userName = $_POST['username'];
 	$password = $_POST['password'];
 
-	require_once('../classes/classes_database.php');
+	require_once(__DIR__ . '/../classes/classes_database.php');
 	error_log("in login.php, user is $userName");
 
 	$userObject = new User($userName, "", $password, "local");
